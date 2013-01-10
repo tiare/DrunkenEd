4f8a9d329972d2189cad085797cf342aed8497d6
Marcel Karsten <marcel.karsten@campus.tu-berlin.de>
2013-01-10 14:00:48 +0100

    added world rotation

--- src/control/states/GameState.java
+++ src/control/states/GameState.java
@@ -6,10 +6,30 @@ import graphics.StandardTextures;
 
 public class GameState extends WorldState {
 
+	private float worldRotation;
+	private float time;
+	
+	public GameState(){
+		super();
+		time = (float)Math.PI/2.0f;
+	}
+	
 	@Override
 	public void onStep(float deltaTime) {
+		//float timeShift = 0.9f + (float)Math.random()/5.0f;
+		
+		//stateTimer;
+		
+		time += deltaTime;//*timeShift;
+		// calculate world rotation while considering difficulty
+		worldRotation += (float)Math.sin(time) / 1.0f; 
+		
 		DrunkenSkeleton skeleton = (DrunkenSkeleton)player.getSkeleton();
+		
+		
+		camera.setRotation(worldRotation);
 		camera.set(skeleton.mHipJoint.mPosX, skeleton.mHipJoint.mPosY, 2);
+		
 		player.step(deltaTime);
 	}
 
@@ -20,7 +40,8 @@ public class GameState extends WorldState {
 		player.draw();
 		
 		graphics.bindTexture(StandardTextures.CUBE);
-		graphics2D.drawRectCentered(0,0.15f, 1,0.9f, player.getSpeed() < 0 ? stateTimer : -stateTimer);
+		graphics2D.drawRectCentered(0,-0.45f, 2,0.9f, 0);
+		//player.getSpeed() < 0 ? stateTimer : -stateTimer
 		
 	}
 	
