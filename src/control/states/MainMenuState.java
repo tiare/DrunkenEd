b21d4e00fd7ea8695c56641831cf3375b22ba035
tiare <tiaref26@gmail.com>
2013-01-31 12:35:33 +0100

    Adapt drinking angles

--- src/control/states/MainMenuState.java
+++ src/control/states/MainMenuState.java
@@ -84,7 +84,7 @@ public class MainMenuState extends WorldState {
 	private float handAngle2 = 287;
 	private float inactiveElbowAngle = 10;
 	private float inactiveShoulderAngle = -15;
-	private float angleIncrease = 2;
+	private float angleIncrease = 5;
 
 	private float activationTime = 0;
 	private float hintTimeout = 5.f;
@@ -217,7 +217,7 @@ public class MainMenuState extends WorldState {
 			// set right drinking arm
 			skeleton.mRightElbowJoint.setPosByAngle((float) Math.toRadians((activeLevel == LEFT) ? shoulderAngle1+increase : shoulderAngle2+increase));
 			skeleton.mRightHandJoint.setPosByAngle((float) Math.toRadians((activeLevel == LEFT) ? elbowAngle1 : elbowAngle2));
-			skeleton.mBottleJoint.setPosByAngle((float) Math.toRadians((activeLevel == LEFT) ? handAngle1 : handAngle2));
+			skeleton.mBottleJoint.setPosByAngle((float) Math.toRadians((activeLevel == LEFT) ? handAngle1+increase : handAngle2+increase*0.45));
 			shadowSkeleton.refreshBottle();
 		}
 	}
